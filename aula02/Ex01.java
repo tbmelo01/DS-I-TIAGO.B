3e907a7439374f57f822251257a17f83a4024c2d
tbmelo01 <batistamelo2012@hotmail.com>
2023-03-17 00:31:31 -0300

    Aula onde aprendi sobre métodos,interface gráfica, e organização de projetos

---
+++ aula02/Ex01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula02;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Ex01 {
+
+    public static void main(String[] args) {
+        String nome = "TB Belo";
+        int idade = 31;
+        System.out.println("Meu apelido é: " + nome + "\n"
+                + "e minha idade é: " + idade);
+
+    }
+
+}
